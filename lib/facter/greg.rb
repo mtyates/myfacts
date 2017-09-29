ac0b8c5b90fe4409e1ed6b45c05b424473ea21b9
Greg Loughmiller <gloughmiller@yahoo.com>
2017-09-29 13:54:10 -0400

    new command

--- lib/facter/greg.rb
+++ lib/facter/greg.rb
@@ -7,7 +7,7 @@ Facter.add('greg') do
   uname_result = nil
   setcode do
     if Facter::Util::Resolution.which('uname')
-      uname_cmd = '/bin/uname -o'
+      uname_cmd = '/bin/hostname -a'
       uname_result = Facter::Util::Resolution.exec(uname_cmd)
     end
     uname_result
