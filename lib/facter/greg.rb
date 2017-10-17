7a42962b1ae3bde2a4fe11f980340f26b5893ea3
Greg Loughmiller <gloughmiller@yahoo.com>
2017-10-17 15:32:05 -0400

    Update greg.rb

--- lib/facter/greg.rb
+++ lib/facter/greg.rb
@@ -8,12 +8,11 @@ Facter.add('fact_registry') do
   fact_registry = 'broke_code'
   begin
     
-    if RUBY_PLATFORM.downcase.include?('mswin') or RUBY_PLATFORM.downcase.include?('mingw32')
       require 'win32/registry'
       Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\Windows NT\CurrentVersion') do |regkey|
          fact_registry = regkey['ProductName']
       end
-    end
+    
   rescue
   end
 
