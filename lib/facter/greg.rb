2af69242a33e7e93f4c82594d12bc8449dc460cb
Greg Loughmiller <gloughmiller@yahoo.com>
2017-10-17 14:02:30 -0400

    Update greg.rb

--- lib/facter/greg.rb
+++ lib/facter/greg.rb
@@ -3,28 +3,14 @@
 #
 # Written to work out the version of windows that the client is currently being run on.
 #
-Facter.add('operatingsystemversion') do
-  confine :kernel => :windows
-  
-  operatingsystemversion = 'unknown'
-  begin
-    
-    if RUBY_PLATFORM.downcase.include?('mswin') or RUBY_PLATFORM.downcase.include?('mingw32')
-      require 'win32/registry'
-    
-      Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\Microsoft\Windows NT\CurrentVersion') do |reg|
-        reg.each do |name,type,data|
-          if name.eql?('ProductName')
-            operatingsystemversion = data
-          end
-        end
-      end
-    end
-  rescue
-
-  end
-
+Facter.add('fact_registry') do
+  confine :osfamily => :windows
   setcode do
-    operatingsystemversion
+    value = nil
+    Win32::Registry::HKEY_LOCAL_MACHINE.open('Software\Puppet Labs\Puppet') do |regkey|
+      value = regkey['RememberInstallDir']
+    end
+    value
   end
 end
+
